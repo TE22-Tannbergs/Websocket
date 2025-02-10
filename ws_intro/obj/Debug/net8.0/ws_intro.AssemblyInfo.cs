62e4f342c2a474c5f99b8aa4387358b883789ac7
Lias <lias.israelsson@edu.lycksele.se>
2025-02-10 12:27:20 +0100

    updated 5

--- ws_intro/obj/Debug/net8.0/ws_intro.AssemblyInfo.cs
+++ ws_intro/obj/Debug/net8.0/ws_intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ws_intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af697bb5a8ec41c4b7c1465cd98f4a79ac9161ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4475a08fe6b6e50c102f6f1f31e607f26cdc15da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ws_intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ws_intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
